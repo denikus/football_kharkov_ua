4e68e26f6688ae43434c89da6587f09fbb66bb04
Denix <denis.soloshenko@gmail.com>
2010-07-06 09:03:27 +0300

    fix rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :validatable
+#  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :validatable
   
   has_one :profile
 
