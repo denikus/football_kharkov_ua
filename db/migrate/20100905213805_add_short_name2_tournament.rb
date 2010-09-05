869842ae7c15e53cc1be2175eb480eed74deb3b4
Denix <denis.soloshenko@gmail.com>
2010-09-06 01:27:02 +0300

    added migrations

---
+++ db/migrate/20100905213805_add_short_name2_tournament.rb
@@ -0,0 +1,9 @@
+class AddShortName2Tournament < ActiveRecord::Migration
+  def self.up
+    add_column :tournaments, :short_name, :string, :limit => 255
+  end
+
+  def self.down
+    remove_column :tournaments, :short_name
+  end
+end
