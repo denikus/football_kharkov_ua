6dde04e82092674d938735fc8f5a12ae6dc93caa
Denix <denis.soloshenko@gmail.com>
2010-09-18 01:19:45 +0300

    some fixes to team list

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
   layout "app_without_sidebar"
   def index
     unless current_subdomain.nil?
-      @teams = Tournament.find_by_url(current_subdomain).seasons.last.teams
+      @teams = Tournament.find_by_url(current_subdomain).seasons.last.teams.find(:all, :order => "name ASC")
     end
   end
 end
