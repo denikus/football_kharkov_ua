a694c1b61c17228e23a47954487d48e98e40d47d
Denix <denis.soloshenko@gmail.com>
2010-09-17 22:58:31 +0300

    added migration for previous commit

---
+++ db/migrate/20100917192510_create_seasons_teams.rb
@@ -0,0 +1,12 @@
+class CreateSeasonsTeams < ActiveRecord::Migration
+  def self.up
+    create_table :seasons_teams, :id => false do |t|
+      t.integer :season_id
+      t.integer :team_id
+    end
+  end
+
+  def self.down
+    drop_table :seasons_teams
+  end
+end
