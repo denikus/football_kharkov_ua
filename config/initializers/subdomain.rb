5668ce471bc56d2e26f668775c58cb7f4e73406c
Denix <denis.soloshenko@gmail.com>
2010-09-26 20:13:04 +0300

    fixed error in footballers url

--- config/initializers/subdomain.rb
+++ config/initializers/subdomain.rb
@@ -1,4 +1,3 @@
 SubdomainFu.tld_sizes = {:development => 0,
                          :test => 0,
                          :production => 2}
-#ActionController::UrlWriter::default_url_options[:host] = 'football.kharkov.ua'
