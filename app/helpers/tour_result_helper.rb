004acbd7d3a281f3111c279893cd46edaea9bf8d
Denix <denis.soloshenko@gmail.com>
2010-09-30 12:13:04 +0300

    some templates for tour result fixed

--- app/helpers/tour_result_helper.rb
+++ app/helpers/tour_result_helper.rb
@@ -5,7 +5,17 @@ module TourResultHelper
     return "#{host.team[:name]} #{host.stats.find_by_name("score").value} : #{guest.stats.find_by_name("score").value} #{guest.team[:name]}"
   end
 
-  def get_score_classes(host_score, guest_score)
-    
+  def get_score_classes(hosts_score, guests_score)
+    winner_class = "fancy-score-winner"
+    loser_class = "fancy-score-loser"
+    style_classes = {:hosts_class => "", :guests_class => ""}
+    if hosts_score > guests_score
+      style_classes[:hosts_class] = winner_class
+      style_classes[:guests_class] = loser_class
+    elsif hosts_score < guests_score
+      style_classes[:hosts_class] = loser_class
+      style_classes[:guests_class] = winner_class
+    end
+    return style_classes
   end
 end
