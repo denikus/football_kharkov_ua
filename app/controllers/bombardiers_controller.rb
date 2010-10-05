befd3952af4d6dd8e79bf24533cf299f6d0e9c33
Denix <denis.soloshenko@gmail.com>
2010-10-06 00:48:56 +0300

    correct table

--- app/controllers/bombardiers_controller.rb
+++ app/controllers/bombardiers_controller.rb
@@ -2,6 +2,7 @@ class BombardiersController < ApplicationController
   layout "app_without_sidebar"
 
   def index
-    
+    ap @stat = Stat.find(:all, :select => "stats.*, COUNT(value) AS goal_count ", :conditions => ["name = ? ", "goal"], :group => "statable_id")
+    debugger
   end
 end
