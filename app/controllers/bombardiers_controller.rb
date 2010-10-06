dca9d6c0eeea01ac430a56c98ae6f55ebbf39390
Denix <denis.soloshenko@gmail.com>
2010-10-06 10:53:45 +0300

    bombardiers list

--- app/controllers/bombardiers_controller.rb
+++ app/controllers/bombardiers_controller.rb
@@ -2,7 +2,21 @@ class BombardiersController < ApplicationController
   layout "app_without_sidebar"
 
   def index
-    ap @stat = Stat.find(:all, :select => "stats.*, COUNT(value) AS goal_count ", :conditions => ["name = ? ", "goal"], :group => "statable_id")
-    debugger
+    @bombardiers = Footballer.paginate(:all,
+                    :select => "`footballers`.*, `football_players`.id AS football_player_id, statistic.statable_total",
+                    :joins => "INNER JOIN `football_players` " +
+                                "ON (footballers.id = football_players.footballer_id) " +
+                              "INNER JOIN (SELECT stats.statable_id, COUNT(stats.value) AS statable_total " + 
+		                                       "FROM`stats` " +
+		                                       "WHERE " +
+		                                       "stats.statable_type='FootballPlayer' AND stats.name='goal' " +
+                                        	 "GROUP BY stats.statable_id) AS statistic " +
+                                "ON (statistic.statable_id=football_players.id)",
+                    :conditions => "footballers.id > 0",
+                    :order => "statistic.statable_total DESC, footballers.last_name ASC",
+                    :per_page => 10,
+                    :page => 1 
+                  )
+    
   end
 end
