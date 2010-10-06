b2a560ae084dc70bf447885a6157de68120fe5aa
Denix <denis.soloshenko@gmail.com>
2010-10-06 11:16:41 +0300

    bombardiers list

--- app/controllers/bombardiers_controller.rb
+++ app/controllers/bombardiers_controller.rb
@@ -13,7 +13,7 @@ class BombardiersController < ApplicationController
                                         	 "GROUP BY stats.statable_id) AS statistic " +
                                 "ON (statistic.statable_id=football_players.id)",
                     :conditions => "footballers.id > 0",
-                    :order => "statistic.statable_total DESC, footballers.last_name ASC",
+                    :order => "statable_sum DESC, footballers.last_name ASC",
                     :per_page => 10,
                     :page => 1 
                   )
