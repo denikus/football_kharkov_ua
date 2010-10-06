684d14168e6d564b63cc8baa2cad0fb58bbb5f7c
Denix <denis.soloshenko@gmail.com>
2010-10-07 01:11:24 +0300

    modification of bombardiers list

--- app/controllers/bombardiers_controller.rb
+++ app/controllers/bombardiers_controller.rb
@@ -15,16 +15,17 @@ class BombardiersController < ApplicationController
                     :conditions => "footballers.id > 0",
                     :group => "footballers.id ",
                     :order => "statable_sum DESC, footballers.last_name ASC",
-                    :having => "statable_sum>2",
                     :per_page => 50,
                     :page => 1 
                   )
     bombardiers_grouped = {}
     @bombardiers.each do |item|
+      if item[:statable_sum].to_i > 2
         if bombardiers_grouped[item[:statable_sum]].nil?
           bombardiers_grouped[item[:statable_sum]] = []
         end
         bombardiers_grouped[item[:statable_sum]] << item
+      end
     end
     @bombardier_list = bombardiers_grouped.sort {|a, b| b[0].to_i <=> a[0].to_i}
 #    @bombardiers.sort {|a, b| a[:statable_sum] <=>}
