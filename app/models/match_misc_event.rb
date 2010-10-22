a79a07745d31d2ad55d72fb08ca077c5a1982173
Denix <denis.soloshenko@gmail.com>
2010-10-23 01:34:28 +0300

    add missing indexes

--- app/models/match_misc_event.rb
+++
@@ -1,3 +0,0 @@
-class MatchMiscEvent < ActiveRecord::Base
-  has_one :match_event, :as => :event, :dependent => :destroy
-end
--- app/models/match_team_event.rb
+++
@@ -1,4 +0,0 @@
-class MatchTeamEvent < ActiveRecord::Base
-  has_one :match_event, :as => :event, :dependent => :destroy
-  belongs_to :competitor
-end
