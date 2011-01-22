e026f878fde93c147fb0569ee2181b095b8c6a92
Denix <denis.soloshenko@gmail.com>
2011-01-22 23:08:37 +0200

    team list by season

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,20 @@ class TeamsController < ApplicationController
   
   def index
     unless current_subdomain.nil?
-      @teams = Tournament.find_by_url(current_subdomain).seasons.last.teams.find(:all, :order => "name ASC")
+      tournament = Tournament.find_by_url(current_subdomain)
+      @teams = Team.find(:all,
+                          :joins => "INNER JOIN `steps_teams` " +
+                                      "ON (teams.id = steps_teams.team_id) " +
+                                    "INNER JOIN `steps` " +
+                                      "ON (steps_teams.step_id = steps.id AND type = 'StepSeason')",
+                          :conditions => ["tournament_id = ? ", tournament.id],
+                          :order => "teams.name ASC"
+                          )
+#      @teams = StepSeason.find(:last,
+#                               :conditions => ["tournament_id = ?", tournament.id],
+#                               :order => "name ASC"
+#                              )
+#      @teams = Tournament.find_by_url(current_subdomain).seasons.last.teams.find(:all, :order => "name ASC")
     end
   end
 
