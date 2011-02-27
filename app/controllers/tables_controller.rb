afa1f3210c992144630eb27575435f798daef023
Denix <denis.soloshenko@gmail.com>
2011-02-27 18:51:34 +0200

    tournament tables

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -2,7 +2,7 @@ class TablesController < ApplicationController
   layout "app_without_sidebar"
   
   def index
-    params[:id] = 10
+#    params[:id] = 10
 #    @set = StepLeague.for_table.find(10).table_set
 #    @set = []
 #    StepLeague.for_table.find(:all, :conditions => {:id => [10,11,12,13,14]}).map(&:last_table).each do |table|
@@ -28,12 +28,20 @@ class TablesController < ApplicationController
 #    ap @set = StepLeague.for_table.find(params[:id]).table_set
 #    puts '---------------------------'
 #    @sets = {:league => StepLeague.find}
-    @leagues = StepLeague.for_table.find([10,11,12,13,14])
+    tournament = Tournament.find_by_url(current_subdomain)
+
+    @stages = tournament.step_seasons.find(:last, :order => "identifier ASC").stages.find(:all, :order => "identifier DESC")
+
+    leagues = []
+    @stages.each{ |stage| stage.leagues.collect{|x| leagues << x.id }}
+
+    @leagues = StepLeague.for_table.find(leagues)
+#    @leagues = StepLeague.for_table.find([10,11,12,13,14])
 #    @set = StepLeague.for_table.find(10).table_set
 #    @sets = []
-#    @leagues.each do |league|
-#      @sets << {:table_set => league.table_set, :league => league}
-#    end
+    @leagues.each do |league|
+      @sets << {:table_set => league.table_set, :league => league}
+    end
 
   end
 end
