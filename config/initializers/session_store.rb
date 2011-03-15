9a52654272b5c7996761faa5e8ad11cb2ecd099f
Denix <denis.soloshenko@gmail.com>
2011-03-15 23:26:23 +0200

    white_list plugin added for comments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 #FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => :all
-FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => :all
+FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => '.football.kharkov.ua'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
