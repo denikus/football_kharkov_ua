5d67568effc9a3a4c70cd6cb1bf3bdc2fdce043c
Denix <denis.soloshenko@gmail.com>
2011-03-16 14:13:52 +0200

    some extentions and lib added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include UrlHelper 
   protect_from_forgery
+
+  protected
+
+  def ext_success data={}
+    {:json => {:success => true}.merge(data)}
+  end
+
+  def ext_failure
+    {:json => {:success => false}}
+  end
 end
---
+++ lib/extensions/paperclip.rb
@@ -0,0 +1,16 @@
+
+module FootballExtensions
+  module Paperclip
+    def self.extended base
+      base.module_eval do
+        class << self
+          def run_with_zero_fix cmd, *params
+            params.each{ |p| p.gsub!(/(\[0\])$/, '') if p.is_a?(String) and p =~ /\[0\]$/ }
+            run_without_zero_fix cmd, *params
+          end
+          alias_method_chain :run, :zero_fix
+        end
+      end
+    end
+  end
+end
