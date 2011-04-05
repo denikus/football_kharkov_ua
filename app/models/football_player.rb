a73545b0f1312e896382dc5e4c4bce113c355ea4
Denix <denis.soloshenko@gmail.com>
2011-04-05 14:04:41 +0300

    missed 10 and 6 meter penalty added and some icons for match

--- app/models/football_player.rb
+++ app/models/football_player.rb
@@ -1,5 +1,5 @@
 class FootballPlayer < ActiveRecord::Base
-  STATS = %w{goal goal_10 goal_6 auto_goal yellow_card red_card}.freeze
+  STATS = %w{goal goal_10 goal_6 auto_goal yellow_card red_card missed_goal_6 missed_goal_10}.freeze
   
   belongs_to :competitor
   belongs_to :footballer
@@ -9,8 +9,28 @@ class FootballPlayer < ActiveRecord::Base
   scope :sort_by_number, :order => "number ASC"
   
   def update stats
+    #check stats and create keys for missed_goal_10, missed_goal_6 if needed
+    full_stats = {}
+    stats.each do |key, val|
+      penalty = {}
+      if ["goal_10", "goal_6"].include?(key)
+        penalty[key] = {:missed => [], :hit => []}
+        val.split(",").each do |stat_item|
+          if !stat_item.index("--").nil?
+            penalty[key][:missed] << stat_item.to_i
+          else
+            penalty[key][:hit] << stat_item.to_i
+          end
+        end
+        full_stats["missed_#{key}"] = penalty[key][:missed].join(",")
+        full_stats[key] = penalty[key][:hit].join(",")
+      else
+        full_stats[key] = val
+      end
+    end
+    
     self.number = stats['number']
-    STATS.each{ |s| self.stats.set s, *stats[s].split(',').map(&:to_i) unless stats[s].empty? }
+    STATS.each{ |s| self.stats.set s, *full_stats[s].split(',').map(&:to_i) unless full_stats[s].empty? }
   end
   
   def match_id
