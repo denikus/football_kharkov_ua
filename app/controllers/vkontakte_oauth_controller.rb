c03e018c7f5508e2b4defa74ceb21f47fe2d21a3
Denix <denis.soloshenko@gmail.com>
2011-05-02 23:45:59 +0300

    test omniauth

--- app/controllers/vkontakte_oauth_controller.rb
+++ app/controllers/vkontakte_oauth_controller.rb
@@ -1,11 +1,13 @@
 class VkontakteOauthController < ApplicationController
 
   def callback
-    access_token = client.web_server.get_access_token(
-      params[:code], :redirect_uri => vkontakte_oauth_url(:action => "callback")
-    )
+#    auth = request.env["rack.auth"]
+    
+#    access_token = client.web_server.get_access_token(
+#      params[:code], :redirect_uri => vkontakte_oauth_url(:action => "callback")
+#    )
+    @user_json = request.env["ominauth.auth"]
 
-    @user_json = access_token.get('/me')
     # in reality you would at this point store the access_token.token value as well as
     # any user info you wanted
 
