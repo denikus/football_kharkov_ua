6016c9da23333aea5059439de2aed77c21ee14f3
Denix <denis.soloshenko@gmail.com>
2011-05-02 22:00:33 +0300

    omniauth for twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, '2304423', 'BPQpptM8Asx45sxdtlhC'
+  provider :vkontakte, '2304423', 'BPQpptM8Asx45sxdtlhC', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
 #require 'oa-oauth'
 #Rails.application.config.middleware.use OmniAuth::Builder do
