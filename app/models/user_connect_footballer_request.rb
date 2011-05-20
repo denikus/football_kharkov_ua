b604f1e8020c1c951d8b4ad4902ad24b433ae6f0
Denix <denis.soloshenko@gmail.com>
2011-05-20 11:39:27 +0300

    fix for scan authentication

--- app/models/user_connect_footballer_request.rb
+++ app/models/user_connect_footballer_request.rb
@@ -1,5 +1,5 @@
 class UserConnectFootballerRequest < ActiveRecord::Base
-  has_attached_file :photo
+  has_attached_file :photo, :path => ":rails_root/public/:class/:attachment/:id/:style_:basename.:extension"
 
   belongs_to :footballer
   belongs_to :user
