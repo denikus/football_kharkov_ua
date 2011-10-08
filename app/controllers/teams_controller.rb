b4b9f09bc570bffe1c7cbdfc5ba56eb64fb792fe
denix <denis.soloshenko@gmail.com>
2011-10-08 23:53:15 +0300

    trackable for authentication enabled

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -36,6 +36,7 @@ class TeamsController < ApplicationController
       @season = StepSeason.find(:last,
                                  :joins => "INNER JOIN `steps_teams` ON (steps.id = steps_teams.step_id) ",
                                  :conditions => ["steps_teams.team_id = ? AND steps.tournament_id = ? ", @team.id, @tournament.id]
+                                 # :order => "steps."
                                 )
 #      @team.seasons.by_tournament(@tournament.id)
       @footballers = Footballer.by_team_step({:team_id => @team.id, :step_id => @season.id} )
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :validatable, :trackable
   
   has_one :profile
 
