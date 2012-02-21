852c999334fc41104b77819accb52e60c53f8cec
denix <denis.soloshenko@gmail.com>
2012-02-21 02:05:09 +0200

    merged with develop

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-require 'generator.rb'
+#require 'generator.rb'
 
 class Statistic < ActiveRecord::Base
   S = Hash[*all.collect{ |s| [s.symbol.to_sym, s.id] }.flatten].freeze rescue {}
@@ -11,7 +11,8 @@ class Statistic < ActiveRecord::Base
       end
       
       define_method(sym.to_s+'=') do |val|
-        SyncEnumerator.new(target.select{ |s| s.statistic_id == S[sym] }, Array(val)).each do |s, v|
+        #SyncEnumerator.new(target.select{ |s| s.statistic_id == S[sym] }, Array(val)).each do |s, v|
+        Enumerator::Generator.new(target.select{ |s| s.statistic_id == S[sym] }, Array(val)).each do |s, v|
           s.destroy and next if v.nil?
           (s || Stat.new(:statistic_id => S[sym], :statable => proxy_owner)).tap{ |s| s.statistic_value = v }.save
         end
