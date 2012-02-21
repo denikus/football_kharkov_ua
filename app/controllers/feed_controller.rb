7a861b11c166e2c2e37877db1e55a45172699f34
denix <denis.soloshenko@gmail.com>
2012-02-21 02:52:47 +0200

    merged with develop

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class FeedController < ApplicationController
   def index
+    request.format = "xml" unless params[:format]
     @posts = Post.paginate :page => 1, :per_page => 10, :order => 'created_at DESC'
     @feed = {:title => "Основная лента"}
     render :layout=>false
