3aeee9d207dec35daca6076984354d09d8acff6d
denix <denis.soloshenko@gmail.com>
2012-02-25 22:55:19 +0200

    cookie problem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,9 @@
 
 #FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => :all
 #FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => '.football.kharkov.ua'
-domain = Rails.env == "production" ? '.football.kharkov.ua' : '.football.lvh.me'
-FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => domain
+#domain = Rails.env == "production" ? '.football.kharkov.ua' : '.football.lvh.me'
+
+FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => :all
 #Rails.application.
 
 # Use the database for sessions instead of the cookie-based default,
