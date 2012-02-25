77cfe46a3a7f47972d77777d49fc2aa16e43ea0c
denix <denis.soloshenko@gmail.com>
2012-02-25 23:06:34 +0200

    cookie problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-//$(document).ajaxSend(function(e, xhr, options) {
-//  var token = $("meta[name='csrf-token']").attr("content");
-//  xhr.setRequestHeader("X-CSRF-Token", token);
-//});
+$(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});
 
 //= require jquery-plugins/jquery.tools.min
 //= require jquery-plugins/jquery.scrollTo-min
