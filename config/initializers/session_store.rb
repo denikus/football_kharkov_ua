a9ef4510c47f35f5291c6a9d1b24780bbfec7946
denix <denis.soloshenko@gmail.com>
2012-02-29 06:36:24 +0200

    session fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,9 @@
 
 #FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => :all
 #FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => '.football.kharkov.ua'
-#domain = Rails.env == "production" ? '.football.kharkov.ua' : '.football.lvh.me'
+domain = Rails.env == "production" ? '.football.kharkov.ua' : '.football.lvh.me'
 
-FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => :all
+FootballKharkov::Application.config.session_store :cookie_store, :key => '_football_kharkov_session', :domain => domain
 #Rails.application.
 
 # Use the database for sessions instead of the cookie-based default,
