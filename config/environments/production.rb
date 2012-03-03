5fea0e62e31bc354157be6974e6a0e48ee445c37
denix <denis.soloshenko@gmail.com>
2012-03-03 15:51:38 +0200

    remove gallery script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ FootballKharkov::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application/schedule/schedule.js jquery-plugins/jquery_form/jquery.form.js application/comment/comment.js ckeditor/init.js jquery-plugins/fancybox/jquery.fancybox-1.3.4.pack.js jquery-plugins/fancybox/jquery.easing-1.3.pack.js shadowbox/adapter/shadowbox-jquery.js user.css)
+  config.assets.precompile += %w( application/schedule/schedule.js jquery-plugins/jquery_form/jquery.form.js application/comment/comment.js ckeditor/init.js jquery-plugins/fancybox/jquery.fancybox-1.3.4.pack.js jquery-plugins/fancybox/jquery.easing-1.3.pack.js user.css)
 
   config.action_mailer.default_url_options = { :host => 'football.kharkov.ua' }
   config.action_mailer.delivery_method = :smtp
