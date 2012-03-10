ada6882ccf209ab2acfaa461409f1ee7e80246eb
denix <denis.soloshenko@gmail.com>
2012-03-10 20:04:14 +0200

    fix edit photo

--- app/models/comment_notify.rb
+++ app/models/comment_notify.rb
@@ -1,13 +1,17 @@
 # -*- encoding : utf-8 -*-
 class CommentNotify < ActionMailer::Base
   def new_comment_email(post, comment, recipient, recipients)
-      recipients recipient
-      bcc recipients
-      from "football.kharkov.ua@gmail.com"
-      subject "Новый комментарий в статье: \"  #{post.title}\" "
-      sent_on Time.now
-      content_type "text/html"
-      body :comment => comment, :post => post
+    @comment = comment
+    @post = post
+    mail(
+      :recipients => recipient,
+      :bcc => recipients,
+      :from => "football.kharkov.ua@gmail.com",
+      :subject => "Новый комментарий в статье: \"  #{post.title}\" ",
+      :sent_on => Time.now,
+      :content_type => "text/html"
+    )
+
   end
 
 end
