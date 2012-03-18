f23447f62da62ea648900f5644dcccb57718c469
denix <denis.soloshenko@gmail.com>
2012-03-19 01:01:33 +0200

    fix auth

--- app/assets/javascripts/admin/temp.js
+++ app/assets/javascripts/admin/temp.js
@@ -1,7 +1,7 @@
-//$(document).ajaxSend(function(e, xhr, options) {
-//  var token = $("meta[name='csrf-token']").attr("content");
-//  xhr.setRequestHeader("X-CSRF-Token", token);
-//});
+$(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});
 football = {
   namespace: function(name, obj) {
     obj = obj || {};
