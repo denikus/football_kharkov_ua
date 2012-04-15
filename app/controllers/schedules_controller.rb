aedbc0e1f02b54d38e5bf70b2eab5d96fce6a10e
denix <denis.soloshenko@gmail.com>
2012-04-15 23:22:14 +0300

    fix quick schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -69,7 +69,7 @@ class SchedulesController < ApplicationController
                                )
     @current_date = @match_date[:match_on].to_s
     unless params[:format] == 'quick_results'
-      ap @template_data = render_to_string(:partial => "day", :locals => {:schedules => @schedules})
+      @template_data = render_to_string(:partial => "day", :locals => {:schedules => @schedules})
 
       render :layout => false, :locals => {:current_date => @match_date[:match_on].to_s}
     else
