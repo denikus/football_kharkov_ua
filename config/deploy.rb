704211be8b9fdd498d9c0b6fcf5938e5ad291935
denix <denis.soloshenko@gmail.com>
2013-07-07 17:06:16 +0300

    assets manifest

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,8 @@ namespace :deploy do
   end
 
   task :update_shared_symlinks do
-    %w(config/database.yml public/ckeditor_assets public/footballers).each do |path|
+    run "mkdir #{File.join(release_path)}/public/assets"
+    %w(config/database.yml public/ckeditor_assets public/footballers public/assets/sources_manifest.yml public/assets/manifest.yml).each do |path|
       run "rm -rf #{File.join(release_path, path)}"
       run "ln -s #{File.join(deploy_to, "shared", path)} #{File.join(release_path, path)}"
     end
