0d90c562531212a705baf75d4bb1d59a66cbe886
denix <denis.soloshenko@gmail.com>
2013-07-07 16:27:16 +0300

    assets manifest

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,10 +73,11 @@ namespace :deploy do
     #run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec /usr/local/rvm/gems/ruby-1.9.3-p125@rails3.2/bin/rake assets:precompile"
     #copy source manifest file here
     path = 'public/assets/sources_manifest.yml'
-    run "ln -s #{File.join(release_path, path)} #{File.join(deploy_to, "shared", path)}"
+    run "ln -s #{File.join(deploy_to, "shared", path)} #{File.join(release_path, path)}"
 
     run "cd #{release_path} && RAILS_ENV=production bundle exec /usr/local/rvm/gems/ruby-1.9.3-p125@rails3.2/bin/rake assets:precompile"
-    run "rm -rf #{File.join(release_path, path)}"
-    run "ln -s #{File.join(deploy_to, "shared", path)} #{File.join(release_path, path)}"
+    #
+    #run "rm -rf #{File.join(release_path, path)}"
+    #run "ln -s #{File.join(deploy_to, "shared", path)} #{File.join(release_path, path)}"
   end
 end
