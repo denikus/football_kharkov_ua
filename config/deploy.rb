dcf2fbbfd356bda7fbe41c33642efbfbc3a1d92c
denix <denis.soloshenko@gmail.com>
2013-07-07 16:13:31 +0300

    assets manifest

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,7 @@ namespace :deploy do
   end
 
   task :update_shared_symlinks do
-    %w(config/database.yml public/ckeditor_assets public/footballers public/assets/sources_manifest.yml).each do |path|
+    %w(config/database.yml public/ckeditor_assets public/footballers).each do |path|
       run "rm -rf #{File.join(release_path, path)}"
       run "ln -s #{File.join(deploy_to, "shared", path)} #{File.join(release_path, path)}"
     end
@@ -71,6 +71,10 @@ namespace :deploy do
 
   task :compile_assets do
     #run "cd #{deploy_to}/current && RAILS_ENV=production bundle exec /usr/local/rvm/gems/ruby-1.9.3-p125@rails3.2/bin/rake assets:precompile"
+    #copy source manifest file here
+    #path = 'public/assets/sources_manifest.yml'
+    #run "ln -s #{File.join(deploy_to, "shared", path)} #{File.join(release_path, path)}"
+
     run "cd #{release_path} && RAILS_ENV=production bundle exec /usr/local/rvm/gems/ruby-1.9.3-p125@rails3.2/bin/rake assets:precompile"
   end
 end
