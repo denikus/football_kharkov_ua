f6fd405abfacc148667c936cd8b24e332ecf9544
denix <denis.soloshenko@gmail.com>
2013-11-06 01:10:16 +0200

    news list in api

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,10 +1,8 @@
 # -*- encoding : utf-8 -*-
 class BlogController < ApplicationController
-#  include ActionController::UrlWriter
-#  default_url_options[:host] = 'localhost:3000'
-#  layout 'application'
+
   def index
-    params[:page] = 1 if !params[:page]
+    params[:page] ||= 1
     @posts = Post.paginate(:page => params[:page], :per_page => 10, :include => :tournament, :order => 'created_at DESC')
   end
  
