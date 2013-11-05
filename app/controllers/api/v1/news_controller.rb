5684d7a79706a9a2b9ad6c9b167813e87900a07b
denix <denis.soloshenko@gmail.com>
2013-11-06 01:10:36 +0200

    news list in api

---
+++ app/controllers/api/v1/news_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::NewsController < ApplicationController
+  before_filter :find_tournament
+
+  def index
+    params[:page] ||= 1
+    params[:per_page] ||= 10
+
+    #@posts = Post.tournament(request.subdomain).paginate(:page => params[:page], :per_page => 10, :order => 'created_at DESC')
+    total_posts = @tournament.posts.count
+    @posts = @tournament.posts.paginate(page: params[:page], per_page: 10).order('created_at DESC').collect{|item|
+            {
+              id: item.id,
+              created_at: item.created_at,
+              updated_at: item.updated_at,
+              title: item.title,
+              author: item.user.username,
+              comments_count: item.comments.count
+            }
+    }
+
+    respond_to do |format|
+      format.json{ render json: {total_news: total_posts, news: @posts} }
+    end
+  end
+
+  #def show
+  #  ap @post = Post.find(params[:id])
+  #  #rescue ActiveRecord::RecordNotFound
+  #  #  error = { :error => "The news you were looking for could not be found."}
+  #  #  respond_with(error, :status => 404)
+  #  puts "!!!!!!!!!!!!!!!"
+  #  ap response = {
+  #      id: @post.id,
+  #      created_at: @post.created_at,
+  #      updated_at: @post.updated_at,
+  #      title: @post.title,
+  #      body: @post.body,
+  #      author: @post.user.username,
+  #      comments_count: @post.comments.count
+  #  }
+  #
+  #
+  #  respond_to do |format|
+  #    format.json{ render json: response }
+  #  end
+  #end
+
+  private
+
+  def find_tournament
+    @tournament = Tournament.find_by_url(params[:tournament_id])
+    rescue ActiveRecord::RecordNotFound
+      error = { :error => "The tournament you were looking for could not be found."}
+      respond_with(error, :status => 404)
+  end
+end
