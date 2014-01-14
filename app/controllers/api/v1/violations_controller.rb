caabe152420184e4b51f578f1ef31a3bbabd40a8
denix <denis.soloshenko@gmail.com>
2014-01-14 10:11:04 +0200

    league_name in schedule + fix violations

--- app/controllers/api/v1/violations_controller.rb
+++ app/controllers/api/v1/violations_controller.rb
@@ -28,9 +28,11 @@ class Api::V1::ViolationsController < Api::V1::BaseController
                                                     "ON (stats.statable_id = f_p.id AND stats.statable_type = 'FootballPlayer' AND stats.name IN ('yellow_card', 'red_card')) "
                                                  ).where(:id => season.id).group("tours.id, stats.id").order("tours.id DESC")
     @violations_by_tour = []
+    i = 0
     violations.each do |item|
-      @violations_by_tour[] = [] if @violations_by_tour[item.tour_name].nil?
-      @violations_by_tour[] << {team_name: item.name, first_name: item.first_name, last_name: item.last_name, stats_name: item.stats_name, tour_name: item.tour_name}
+      @violations_by_tour[i] = [] if @violations_by_tour[i].nil?
+      @violations_by_tour[i] << {team_name: item.name, first_name: item.first_name, last_name: item.last_name, stats_name: item.stats_name, tour_name: item.tour_name}
+      i += 1
     end
 
     respond_to do |format|
