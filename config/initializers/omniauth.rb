9e4cf6150537920982bd0f4e2aa9f674437b0072
denix <denis.soloshenko@gmail.com>
2014-09-18 13:48:41 +0300

    fix vk

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, '2304423', 'BPQpptM8Asx45sxdtlhC', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
-  provider :facebook, '88a23ef766664f3380d02de4536980fa', '7731cf1474f8e9ae4e0080407918aebd', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
+  provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET'], :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
+  provider :facebook, ENV['FB_API_KEY'], ENV['VK_API_SECRET'], :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
 #require 'oa-oauth'
 #Rails.application.config.middleware.use OmniAuth::Builder do
